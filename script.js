66fe6368a7a09e7bb25e6256167c2c3157eb9973
PikapikaHei <pikachuhayden1106@gmail.com>
2025-10-16 21:04:00 +0800

    added smth cuz why not

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const player = {
 }
 
 function foo() {
-    console.log(`I am ran! You have ${player.tokens} eugene tokens!!!`)
+    console.log(`I am ran! You have ${format(player.tokens)} eugene tokens!!!`);
     return 0;
 }
+
+function bar(arg) {
+    console.log(`Hey! I found this element for you: ${getElement("eugene")}!`);
+    changeElement("eugene", arg);
+    return 0;
+}
+
+/*
+Hi, I am a comment!
+Once we start working on this, the foo() and bar() shall be deleted.
+They are only for reference.
+*/
