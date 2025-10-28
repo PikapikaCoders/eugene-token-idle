493ddada8dee4678808ef4c7b80cfd25241db71e
PikapikaHei <pikachuhayden1106@gmail.com>
2025-10-28 21:32:50 +0800

    added scam() and upgradeBuy()

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 const player = {
-    tokens: new Decimal(0)
+    tokens: new Decimal(0),
+    money: new Decimal(0),
+    
+    scamming: true,
+    ups: {
+        _total: new Decimal(0),
+
+        yield: new Decimal(0),
+        speed: new Decimal(0),
+        transFmla: new Decimal(0),
+    }
 }
+
+function scam() {
+    if (!player.scamming) { return; }
+    let gain = Decimal.pow(1.4, player.ups.yield);
+
+    let speedUp = Decimal.pow(1.4, player.ups.speed);
+    let timeout = Decimal.div(1e3, speedUp);
+
+    if (timeout < 50) {
+        timeout = 50;
+        gain = gain.times(speedUp.div(20));
+    }
+    
+    player.token += gain;
+    setTimeout(scam, timeout);
+}
+
+function upgradeBuy(property) {
+    let costMult;
+    if (property != "transFmla") { costMult = 1.5; }
+    else { costMult = 1.65; }
+
+    let cost = Decimal.pow(costMult, player.ups._total).times(3);
+    if (player.money.gte(cost)) {
+        player.money = player.money.sub(cost);
+        player.ups[property] = player.ups[property].add(1);
+        player.ups._total = player.ups._total.add(1);
+    } else {
+        alert("Not enough money!");
+    }
+}
+
+/*
+Hi, I am a comment!
+The foo and bar functions are only for reference.
+*/
 
 function foo() {
     console.log(`I am ran! You have ${format(player.tokens)} eugene tokens!!!`);
@@ -12,9 +58,3 @@ function bar(arg) {
     changeElement("eugene", arg);
     return 0;
 }
-
-/*
-Hi, I am a comment!
-Once we start working on this, the foo() and bar() shall be deleted.
-They are only for reference.
-*/
