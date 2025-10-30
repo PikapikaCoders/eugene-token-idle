0e2a9210a650ac62406d06c0bef25f69ff2db59b
PikapikaHei <pikachuhayden1106@gmail.com>
2025-10-30 23:43:12 +0800

    added prestige, does nothing now

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const player = {
     tokens: new Decimal(0),
     money: new Decimal(10),
-
     vel: new Decimal(0),
+
+    prestige: new Decimal(0),
+    prestigeTkn: new Decimal(0),
+    prestigeTknGain: new Decimal(0),
+    prestigeTKnBest: new Decimal(0),
     
     scamming: false,
     scamTimeouted: false,
@@ -68,6 +72,8 @@ function scam() {
 }
 
 function upgradeBuy(property, amount) {
+    if (player.scamming) { return; }
+
     let costMult;
     if (property != "trans") { costMult = 1.5; }
     else { costMult = 1.65; }
@@ -88,6 +94,8 @@ function upgradeBuy(property, amount) {
 }
 
 function upgradeRespec(property, amount) {
+    if (player.scamming) { return; }
+
     let costArr = player.ups[`${property}Cost`];
 
     let max = amount;
@@ -101,6 +109,29 @@ function upgradeRespec(property, amount) {
         player.ups[property] = player.ups[property].sub(1);
         player.ups._total = player.ups._total.sub(1);
     }
+}
+
+function prestige(gaining = true) {
+    if (gaining) {
+        if (player.money.lt(1e3)) { return; }
+
+        player.prestige = player.prestige.add(1);
+        player.prestigeTkn = player.prestigeTkn.add(player.prestigeTknGain);
+        player.prestigeTknBest = Decimal.max(player.prestigeTknBest, player.prestigeTkn);
+    }
+
+    player.tokens = new Decimal(0);
+    player.money = new Decimal(10);
+    player.vel = new Decimal(0);
+    player.scamming = false;
+    player.scamTimeouted = false;
+    player.ups._total = new Decimal(0);
+    player.ups.accel = new Decimal(0);
+    player.ups.speed = new Decimal(0);
+    player.ups.trans = new Decimal(0);
+    player.ups.accelCost = [];
+    player.ups.speedCost = [];
+    player.ups.transCost = [];
 }
 
 setInterval(update, 1);
@@ -118,4 +149,24 @@ function update() {
 
     changeElement("amount", `You have ${format(player.tokens)} eugene tokens & ${format(player.money)} money`);
     changeElement("scam", `SCAM (Scamming: ${player.scamming} | Cooldown: ${player.scamTimeouted})`);
+
+    let desc = [];
+    if (player.scamming) {
+        for (let i=0; i<3; i++) { desc[i] = "[No upgrades when scamming!]"; }
+    } else {
+        desc[0] = "x1.04 Eugene Token acceleration";
+        desc[1] = "x1.4 Eugene Token speed";
+        desc[2] = "x1.4 translation formula";
+    }
+
+    changeElement("accelDesc", desc[0]);
+    changeElement("speedDesc", desc[1]);
+    changeElement("transDesc", desc[2]);
+
+    let prestigeTknGain = new Decimal(0); //TO-DO
+
+    player.prestigeTknGain = prestigeTknGain;
+
+    changeElement("prestigeAmount", `You have ${format(player.prestigeTkn)} prestige tokens.`)
+    changeElement("prestigeGain", `Prestige to gain ${format(player.prestigeTknGain)} prestige tokens.`)
 }
